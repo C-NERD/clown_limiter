3044905d16a07e2dead86331815841f9e7bd4133
C-NERD <alayaa694@gmail.com>
2022-09-02 20:58:59 +0100

    implemented code to give user more control on endpoints to limit and how to limit them

--- src/clown_limiter.nim
+++ src/clown_limiter.nim
@@ -1,27 +1,60 @@
 import jester, clown_limiter/tracker
-from re import re
+from re import re, contains, Regex
 
 export tracker, re
+
+## sequence of data for endpoints which would be limited
+## the sequence contains tuples with endpoint pattern, rate and freq in seconds
+## do not mutate the variable limiter_data directly but instead use the
+## addLimiterEndpoints procedure. Seriously don't mutate this variable directly 👀👀
+var clown_limiter_data_do_not_touch* : seq[tuple[pattern : Regex, rate, freq : int]] = @[
+    (re".+", 50, 60)
+]
+
+proc addLimiterEndpoints*(data : seq[tuple[pattern : Regex, rate, freq : int]]) =
+    ## sets the data for api endpoints to be rate limited.
+    ## this procedure directly mutates a variable so avoid calling it inside of a threaded procedure
+
+    clown_limiter_data_do_not_touch = data
 
 router clown_limiter:
 
     before re".+":
         ## rate limiting endpoint
         
-        let 
-            ip = request.ip()
-            rateinfo = ip.rateStatus()
-            
-        case rateinfo.status
+        {.cast(gcsafe).}:
+            let data = clown_limiter_data_do_not_touch
 
-        of Exceeded:
+        var 
+            check_rate : bool = false
+            rate, freq : int
+        let url = request.path()
+        for info in data:
 
-            halt Http429
+            if contains(url, info.pattern):
 
-        of NotExceeded:
+                check_rate = true
+                rate = info.rate
+                freq = info.freq
+                break
+        
+        if check_rate:
 
-            ip.recordReqRate(rateinfo.calls)
+            let 
+                ip = request.ip()
+                rateinfo = ip.rateStatus(rate, freq)
+                
+            case rateinfo.status
 
-        of Expired:
+            of Exceeded:
 
-            ip.resetReqRate()
+                halt Http429
+
+            of NotExceeded:
+
+                ip.recordReqRate(rateinfo.calls)
+
+            of Expired:
+
+                ip.resetReqRate()
+
